2cb756ae167473922104d1601bc2f7bb54e7cab9
Yinan Hu <besthynlove@gmail.com>
2024-03-05 22:41:18 -0500

    m

--- lc_5_Longest_Palindromic_Substring.py
+++ lc_5_Longest_Palindromic_Substring.py
@@ -1,4 +1,5 @@
 class Solution:
+    # center expansion technique
     # o(n2), o(1)
     def longestPalindrome(self, s: str) -> str:
         start, end = 0, 0
@@ -24,3 +25,29 @@ class Solution:
 
         # Python's slicing includes the start index and excludes the end index, similar to Java's substring
         return s[start:end]
+    
+    # sliding window
+    # o(n2), o(1)
+    def longestPalindrome2(self, s: str) -> str:
+        if not s:
+            return ""
+
+        def expandAroundCenter(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1:right]
+
+        longest = ""
+        for i in range(len(s)):
+            # Odd length palindromes (expand around one center)
+            odd_pal = expandAroundCenter(i, i)
+            if len(odd_pal) > len(longest):
+                longest = odd_pal
+
+            # Even length palindromes (expand around two centers)
+            even_pal = expandAroundCenter(i, i+1)
+            if len(even_pal) > len(longest):
+                longest = even_pal
+
+        return longest
